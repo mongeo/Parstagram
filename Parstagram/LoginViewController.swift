3111108b614d93b56b6ca86c446b2ce087238c25
Geoffrey Montague <gmontague@csumb.edu>
2019-03-03 21:24:41 -0800

    Parstagram part 1 finished!

--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
@@ -19,7 +20,35 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        PFUser.logInWithUsername(inBackground: username, password: password) {
+            (user,error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSeque", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+        
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSeque", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
 
+    }
+    
     /*
     // MARK: - Navigation
 
